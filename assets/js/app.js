6f161b5b560cd6d099f46c21a229c55707216abb
zodwo <senanm260@gmail.com>
2023-06-22 03:12:14 +0400

    scetion 2 fixed

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+(function () {
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    if (this.document.documentElement.scrollTop > 50) {
+      header.style.backgroundColor = "#020303d3";
+    } else {
+      header.style.backgroundColor = "transparent";
+    }
+  });
+})();
