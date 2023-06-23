89fc32f69bddec8bf0b758aa3ca31fa2dd0402c3
zodwo <senanm260@gmail.com>
2023-06-23 05:53:51 +0400

    add new slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 (function () {
   window.addEventListener("scroll", function () {
     const header = document.querySelector(".header");
-    if (this.document.documentElement.scrollTop > 50) {
+    if (this.document.documentElement.scrollTop > 100) {
       header.style.backgroundColor = "#020303d3";
     } else {
       header.style.backgroundColor = "transparent";
---
+++ assets/js/slider.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: "auto",
+  spaceBetween: 60,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
