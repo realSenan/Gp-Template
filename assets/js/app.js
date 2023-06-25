1bdbae52d52a8ea8bab7d0d5457ec1921858837a
zodwo <senanm260@gmail.com>
2023-06-25 21:20:59 +0400

    last fix mobile menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,45 @@
     }
   });
 
+  const closeButton = document.querySelector(".close");
+  const hamburgerButton = document.querySelector(".bars");
+  const menu = document.querySelector(".center");
 
+  hamburgerButton.addEventListener("click", () => {
+    menu.style.transform = "translateX(0)";
+  });
 
+  closeButton.addEventListener("click", () => {
+    menu.style.transform = "translateX(100%)";
+  });
+
+  const deepDropDown = document.querySelector(".ci2-drop-down");
+  const deepDropDownMENU = document.querySelector(".deep-drop-down-menu");
+
+  const dropWRAPPER = document.querySelector("#x");
+  const deepWRAPPER = document.querySelector("#l");
+
+  let counter = 0;
+
+  dropWRAPPER.addEventListener("click", (e) => {
+    counter++;
+    
+    if (!e.target.classList.contains("active")) {
+      dropWRAPPER.classList.add("active");
+    } else {
+    }
+
+    if (counter == 3) {
+      dropWRAPPER.classList.remove("active");
+      counter = 0
+    }
+  });
+
+  deepWRAPPER.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("activex")) {
+      deepWRAPPER.classList.add("activex");
+    } else {
+      deepWRAPPER.classList.remove("activex");
+    }
+  });
 })();
